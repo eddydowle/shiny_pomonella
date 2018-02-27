17467a23342aa703f50f18aba3ac3b32c22f95d2
eddydowle <eddy.dowle@otago.ac.nz>
2018-02-27 17:28:40 +1300

    Making a slider for FDR values this one wont have modules as they are all done at 0.05 FDR so cant work with the slider

--- ShinyAppPvaluesPathways.R
+++ ShinyAppPvaluesPathways.R
@@ -37,39 +37,27 @@ library(reshape2)
 #to start with lets just work on the RNAtable
 #WithinMonthsDE<-read.table("/media/raglandlab/ExtraDrive1/RpomDiapauseRNAseqTraj_GJR/RNAseqToMysql/PomModsDEwithinMonth.csv",header=T,row.names=1,sep=",",stringsAsFactors = F)
 #my connection through uni and the vpn is super slow so Im going to start with running on my machine so I can work out shiny 
-WithinMonthsDE<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/shiny/PomModsDEwithinMonth.csv",header=T,row.names=1,sep=",",stringsAsFactors = F)
-WithinMonthsDE.haw<-data.frame(WithinMonthsDE[1:6])
-WithinMonthsDE.apple<-data.frame(WithinMonthsDE[c(1,2,7:11)])
+apple<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/Table.apple.JuneEJD",header=T,row.names=1,sep="\t",stringsAsFactors = F)
+colnames(apple)
+WithinMonthsDE.apple<-data.frame(apple[c(1:5,44)])
+haw<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/Table.haw.JuneEJD",header=T,row.names=1,sep="\t",stringsAsFactors = F)
+colnames(haw)
+WithinMonthsDE.haw<-data.frame(haw[c(33,1:4,9)])
+WithinMonthsDE.apple<-tibble::rownames_to_column(WithinMonthsDE.apple, "gene_id")
+WithinMonthsDE.haw<-tibble::rownames_to_column(WithinMonthsDE.haw, "gene_id")
 WithinMonthsDE.haw$month2vs2_haw_logFC.x<-0
-WithinMonthsDE.haw<-WithinMonthsDE.haw[,c(1,2,7,3,4,5,6)]
-WithinMonthsDE.apple<-WithinMonthsDE.apple[,c(1,2,3,4,5,6,7)]
-colnames(WithinMonthsDE.haw) <- c("gene_id","flybase", "2M","3M","4M","5M","6M")
-colnames(WithinMonthsDE.apple) <- c("gene_id", "flybase", "2M","3M","4M","5M","6M")
-WithinMonthsDE.haw.melt <- melt(WithinMonthsDE.haw,  c("gene_id","flybase"))
-WithinMonthsDE.apple.melt <- melt(WithinMonthsDE.apple, c("gene_id","flybase"))
+WithinMonthsDE.apple$month2vs2_apple_logFC.x<-0
+WithinMonthsDE.haw<-WithinMonthsDE.haw[,c(1,2,8,3,4,5,6,7)]
+WithinMonthsDE.apple<-WithinMonthsDE.apple[,c(1,2,8,3,4,5,6,7)]
+colnames(WithinMonthsDE.haw) <- c("gene_id","flybase", "2M","3M","4M","5M","6M","FDR")
+colnames(WithinMonthsDE.apple) <- c("gene_id", "flybase", "2M","3M","4M","5M","6M","FDR")
+WithinMonthsDE.haw.melt <- melt(WithinMonthsDE.haw,  c("gene_id","flybase","FDR"))
+WithinMonthsDE.apple.melt <- melt(WithinMonthsDE.apple, c("gene_id","flybase","FDR"))
 WithinMonthsDE.haw.melt$pop<-"haw"
 WithinMonthsDE.apple.melt$pop<-"apple"
 #together<-rbind(WithinMonthsDE.haw.melt,WithinMonthsDE.apple.melt)
 together<-rbind(WithinMonthsDE.apple.melt,WithinMonthsDE.haw.melt)
 
-#Across months:
-#WithinMonthsDE<-read.table("/media/raglandlab/ExtraDrive1/RpomDiapauseRNAseqTraj_GJR/RNAseqToMysql/PomModsDEacrossMonth.csv",header=T,row.names=1,sep=",",stringsAsFactors = F)
-#my connection through uni and the vpn is super slow so Im going to start with running on my machine so I can work out shiny 
-AcrossMonthsDE<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/shiny/PomModsDEacrossMonth.csv",header=T,row.names=1,sep=",",stringsAsFactors = F)
-colnames(AcrossMonthsDE)
-AcrossMonthsDE.haw<-data.frame(AcrossMonthsDE[c(1,12,2:6)])
-AcrossMonthsDE.apple<-data.frame(AcrossMonthsDE[c(1,12,2,7:11)])
-AcrossMonthsDE.haw$month2vs2_haw_logFC.x<-0
-AcrossMonthsDE.haw<-AcrossMonthsDE.haw[,c(1,2,3,8,4,5,6,7)]
-AcrossMonthsDE.apple<-AcrossMonthsDE.apple[,c(1,2,3,4,5,6,7,8)]
-colnames(AcrossMonthsDE.haw) <- c("gene_id","module","flybase", "2M","3M","4M","5M","6M")
-colnames(AcrossMonthsDE.apple) <- c("gene_id", "module","flybase", "2M","3M","4M","5M","6M")
-AcrossMonthsDE.haw.melt <- melt(AcrossMonthsDE.haw,  c("gene_id","flybase","module"))
-AcrossMonthsDE.apple.melt <- melt(AcrossMonthsDE.apple, c("gene_id","flybase","module"))
-AcrossMonthsDE.haw.melt$pop<-"haw"
-AcrossMonthsDE.apple.melt$pop<-"apple"
-#together<-rbind(WithinMonthsDE.haw.melt,WithinMonthsDE.apple.melt)
-together.across<-rbind(AcrossMonthsDE.apple.melt,AcrossMonthsDE.haw.melt)
 
 
 #pathways
@@ -78,11 +66,11 @@ tor<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/Apple
 insulin<-read.table("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/shiny/insulinSignalingFlybase.txt",header=T,row.names=NULL,sep="\t",stringsAsFactors = F)
 
 
-together.across %>% distinct(., module)
+#together.across %>% distinct(., module)
 #https://stackoverflow.com/questions/48565661/dynamically-adding-and-removing-objects-in-response-to-selectinput-in-shiny
 
 #thinking about:
-#put a slider for pvalue
+#put a slider for FDR
 #do network plots and then change highlighting?? with radio buttons??
 #pathway package in R kegg pathways uses flybase IDs I think
 
@@ -91,80 +79,94 @@ together.across %>% distinct(., module)
 
 ui<-fluidPage(
   conditionalPanel(condition="input.conditionedPanels==1",
+                   sliderInput("integer", "FDR:",
+                               min = 0, max = 1,
+                               value = 0.05),
                    radioButtons('across', label = 'pathway_across',
                                 choices = c("all","wnt","insulin","tor"),selected='all'),
                    uiOutput(outputId = "gene")),
   conditionalPanel(condition="input.conditionedPanels==2",
-                   radioButtons('across_modules', label = 'Modules',
-                                choices = c("1","2","3","4","5","6","7"),selected="1"),
-                   uiOutput(outputId = "gene_module"))
-  
-,
+                   sliderInput("integer", "FDR:",
+                               min = 0, max = 1,
+                               value = 0.05),
+                   radioButtons('across', label = 'pathway_across',
+                                choices = c("all","wnt","insulin","tor"),selected='all'),
+                   uiOutput(outputId = "gene2")),
   mainPanel(
-        h4("Significant Across Months"),
-        tabsetPanel(
-          tabPanel(
-            h4("Pathways"),
-            plotOutput(outputId = "main_plot") ,
-            #define the value of this tab
-            value=1 #value relates to 'id=' in tabsetpanel. Except to get a value and relates that to'id' in tabsetPanel
-          ),
-          #add a tab for the boxplot
-          tabPanel(
-            h4("Modules"),
-            plotOutput(outputId = "second_plot") ,
-            #define the value of this tab (not necessary here-just example)
-            value=2
-          )
-          #name the panel to correspond ot the condition defined in the sidebarpanel
-          , id='conditionedPanels'
-        )
+    h4("Significant Across Months"),
+    tabsetPanel(
+      tabPanel(
+        h4("Pathways DE within months FDR"),
+        plotOutput(outputId = "main_plot") ,
+        #define the value of this tab
+        value=1 #value relates to 'id=' in tabsetpanel. Except to get a value and relates that to'id' in tabsetPanel
+      ),
+      #add a tab for the boxplot
+      tabPanel(
+        h4("Pathwyas DE across months FDR"),
+        plotOutput(outputId = "second_plot") ,
+        #define the value of this tab (not necessary here-just example)
+        value=2
+      )
+      #name the panel to correspond ot the condition defined in the sidebarpanel
+      , id='conditionedPanels'
+    )
   )
 )
 
 #create the function to be excuted by shiny
 server<-function(input,output) {
- # output$menuitem<-renderMenu({
-    
+  # output$menuitem<-renderMenu({
+  
   #})
   output$gene <- renderUI({
-    df<-together.across
+    df<-together
     #test<-'wnt'
     if (input$across=='wnt'){
-      choicesare<-df %>% filter(., flybase %in% wnt$flyid) %>% distinct(.,gene_id)}
+      choicesare<-df %>% filter(., flybase %in% wnt$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) 
+      }
     if(input$across=='tor'){
-    choicesare<-df %>% filter(., flybase %in% tor$flyid) %>% distinct(.,gene_id)}
+      choicesare<-df %>% filter(., flybase %in% tor$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) }
     if(input$across=='insulin'){
-    choicesare<-df %>% filter(., flybase %in% insulin$flyid) %>% distinct(.,gene_id)}
+      choicesare<-df %>% filter(., flybase %in% insulin$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) }
     if(input$across=='all'){
-    choicesare<-df %>% distinct(.,gene_id)}
- 
-       selectizeInput(inputId = "plot_var",
-                   label= "variable to plot",
- #                  choices=together.across %>% distinct(.,gene_id), 
-                    choices=choicesare,
-                   selected='gene10053',
-                   options=list(maxOptions=3000))
+      choicesare<-df %>% filter(.,FDR < input$integer) %>% distinct(.,gene_id)}
     
-  })
-  output$gene_module <- renderUI({
-    df<-together.across
-    
-    #test<-'wnt'
-    selectizeInput(inputId = "plot_var2",
+    selectizeInput(inputId = "plot_var",
                    label= "variable to plot",
                    #                  choices=together.across %>% distinct(.,gene_id), 
-                   choices=together.across %>% filter(., module %in% input$across_modules) %>% distinct(.,gene_id),
+                   choices=choicesare,
+                   selected='gene10053',
                    options=list(maxOptions=3000))
   })
-
   
-
+  output$gene2 <- renderUI({
+    df<-together
+    #test<-'wnt'
+    if (input$across=='wnt'){
+      choicesare<-df %>% filter(., flybase %in% wnt$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) 
+    }
+    if(input$across=='tor'){
+      choicesare<-df %>% filter(., flybase %in% tor$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) }
+    if(input$across=='insulin'){
+      choicesare<-df %>% filter(., flybase %in% insulin$flyid)%>% filter(.,FDR < input$integer) %>% distinct(.,gene_id) }
+    if(input$across=='all'){
+      choicesare<-df %>% filter(.,FDR < input$integer) %>% distinct(.,gene_id)}
+    
+    selectizeInput(inputId = "plot_var",
+                   label= "variable to plot",
+                   #                  choices=together.across %>% distinct(.,gene_id), 
+                   choices=choicesare,
+                   selected='gene10053',
+                   options=list(maxOptions=3000))
+  })
+  
+  
   output$main_plot <-renderPlot({
     #specify the plot that will be generated
-    together.across.test<-together.across %>% filter(.,gene_id==input$plot_var)
-    title<-together.across.test[1,2]
-    ggplot(together.across.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
+    together.test<-together %>% filter(.,gene_id==input$plot_var)
+    title<-together.test[1,2]
+    ggplot(together.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
       geom_line() +
       ggtitle("Across Months",title) +
       scale_x_discrete("Time series from 2M",labels=c('2M','3M','4M','5M', '6M')) + #discrete
@@ -174,9 +176,9 @@ server<-function(input,output) {
   
   output$second_plot <-renderPlot({
     #specify the plot that will be generated
-    together.across.test<-together.across %>% filter(.,gene_id==input$plot_var2)
-    title<-together.across.test[1,2]
-    ggplot(together.across.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
+    together.test<-together %>% filter(.,gene_id==input$plot_var)
+    title<-together.test[1,2]
+    ggplot(together.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
       geom_line() +
       ggtitle("Across Months",title) +
       scale_x_discrete("Time series from 2M",labels=c('2M','3M','4M','5M', '6M')) + #discrete
