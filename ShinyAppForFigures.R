492ab9f5bc53e531ee0747ebf0decb53ee71a9aa
eddydowle <eddy.dowle@otago.ac.nz>
2018-02-28 17:09:28 +1300

    Fixed the common name showing up on the drop down menu issues. Thinking about more annotations...

--- ShinyAppForFigures.R
+++ ShinyAppForFigures.R
@@ -82,10 +82,11 @@ together.across %>% distinct(., module)
 #https://stackoverflow.com/questions/48565661/dynamically-adding-and-removing-objects-in-response-to-selectinput-in-shiny
 
 #thinking about:
-#put a slider for pvalue
 #put a table in the modules side that has enriched pathways
-#do network plots and then change highlighting?? with radio buttons??
-#pathway package in R kegg pathways uses flybase IDs I think
+
+
+flybase_symbol_ID<-read.csv("/Users/edwinadowle/Documents/Cerasi/Pomonella/RSEMresults/AppleBackToHaw2MremoveBadHaw4M/shiny/fbgn_annotation_ID_fb_2017_06.tsv",header=T,row.names=NULL,sep="\t",stringsAsFactors = F,strip.white=T)
+flybase_symbol_ID<-flybase_symbol_ID[c(1,3)]
 
 
 #link to mysql
@@ -133,13 +134,13 @@ server<-function(input,output) {
     df<-together.across
     #test<-'wnt'
     if (input$across=='wnt'){
-      choicesare<-df %>% filter(., flybase %in% wnt$flyid) %>% distinct(.,gene_id)}
+      choicesare<-df %>% filter(., flybase %in% wnt$flyid) %>% distinct(.,gene_id,.keep_all = TRUE) %>% select(.,gene_id,flybase) %>% left_join(.,flybase_symbol_ID,by="flybase") %>% select(.,gene_id,gene_symbol,flybase) %>% transmute(.,choice=paste(gene_id,gene_symbol,flybase))}
     if(input$across=='tor'){
-    choicesare<-df %>% filter(., flybase %in% tor$flyid) %>% distinct(.,gene_id)}
+    choicesare<-df %>% filter(., flybase %in% tor$flyid) %>% distinct(.,gene_id,.keep_all = TRUE) %>% select(.,gene_id,flybase) %>% left_join(.,flybase_symbol_ID,by="flybase") %>% select(.,gene_id,gene_symbol,flybase) %>% transmute(.,choice=paste(gene_id,gene_symbol,flybase))}
     if(input$across=='insulin'){
-    choicesare<-df %>% filter(., flybase %in% insulin$flyid) %>% distinct(.,gene_id)}
+    choicesare<-df %>% filter(., flybase %in% insulin$flyid) %>% distinct(.,gene_id,.keep_all = TRUE) %>% select(.,gene_id,flybase) %>% left_join(.,flybase_symbol_ID,by="flybase") %>% select(.,gene_id,gene_symbol,flybase) %>% transmute(.,choice=paste(gene_id,gene_symbol,flybase))}
     if(input$across=='all'){
-    choicesare<-df %>% distinct(.,gene_id)}
+    choicesare<-df %>% distinct(.,gene_id,.keep_all = TRUE) %>% select(.,gene_id,flybase) %>% left_join(.,flybase_symbol_ID,by="flybase") %>% select(.,gene_id,gene_symbol,flybase) %>% transmute(.,choice=paste(gene_id,gene_symbol,flybase))}
  
        selectizeInput(inputId = "plot_var",
                    label= "variable to plot",
@@ -156,7 +157,7 @@ server<-function(input,output) {
     selectizeInput(inputId = "plot_var2",
                    label= "variable to plot",
                    #                  choices=together.across %>% distinct(.,gene_id), 
-                   choices=together.across %>% filter(., module %in% input$across_modules) %>% distinct(.,gene_id),
+                   choices=together.across %>% filter(., module %in% input$across_modules) %>% distinct(.,gene_id,.keep_all = TRUE) %>% select(.,gene_id,flybase) %>% left_join(.,flybase_symbol_ID,by="flybase") %>% select(.,gene_id,gene_symbol,flybase) %>% transmute(.,choice=paste(gene_id,gene_symbol,flybase)),
                    options=list(maxOptions=3000))
   })
 
@@ -164,25 +165,26 @@ server<-function(input,output) {
 
   output$main_plot <-renderPlot({
     #specify the plot that will be generated
-    together.across.test<-together.across %>% filter(.,gene_id==input$plot_var)
+    together.across.test<-together.across %>% filter(.,gene_id==strsplit(input$plot_var," +")[[1]][1])
     title<-together.across.test[1,2]
     ggplot(together.across.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
       geom_line() +
       ggtitle("Across Months",title) +
       scale_x_discrete("Time series from 2M",labels=c('2M','3M','4M','5M', '6M')) + #discrete
-      scale_y_continuous("Log Fold Change",limits = c(-3.5, 3.5)) + #continuous
+      scale_y_continuous("Log Fold Change",limits = c(-4, 4)) + #continuous
       theme_bw()
   })
   
   output$second_plot <-renderPlot({
     #specify the plot that will be generated
-    together.across.test<-together.across %>% filter(.,gene_id==input$plot_var2)
+    together.across.test<-together.across %>% filter(.,gene_id==strsplit(input$plot_var2," +")[[1]][1])
     title<-together.across.test[1,2]
     ggplot(together.across.test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
       geom_line() +
       ggtitle("Across Months",title) +
       scale_x_discrete("Time series from 2M",labels=c('2M','3M','4M','5M', '6M')) + #discrete
-      scale_y_continuous("Log Fold Change",limits = c(-3.5, 3.5)) + #continuous
+      scale_y_continuous("Log Fold Change") + #continuous
+      coord_cartesian(ylim = c(-4, 4)) +
       theme_bw()
   })
   
@@ -193,6 +195,13 @@ shinyApp(ui=ui,server=server)
 
 #click run app botton on rstudio
 
+ggplot(test, aes(variable, value,group = interaction(gene_id,pop) ,colour=pop)) +
+  geom_line() +
+  ggtitle("Across Months") +
+  scale_x_discrete("Time series from 2M",labels=c('2M','3M','4M','5M', '6M')) + #discrete
+  scale_y_continuous("Log Fold Change") + #continuous
+  coord_cartesian(ylim = c(-4, 4)) +
+  theme_bw()
 
 
 
